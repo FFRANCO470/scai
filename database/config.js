9e15ca8176fcf52e3206bdf761591735ead35f02
unknown <fjfranco697@misena.edu.co>
2022-04-17 18:51:24 -0500

    local

--- database/config.js
+++ database/config.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const dbConnection = async()=>{
     try{
-        await mongoose.connect(process.env.MONGOOSE_CNX,{
-        // await mongoose.connect(process.env.MONGO_LOCAL,{
+        //await mongoose.connect(process.env.MONGOOSE_CNX,{
+        await mongoose.connect(process.env.MONGO_LOCAL,{
             useNewUrlParser:true,
             useUnifiedTopology:true,
             useCreateIndex:true,
